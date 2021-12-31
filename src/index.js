9d2040975c61e427b65b060cc944c6132bcea3f0
benyamin-nayudi <benyamin.naudi@gmail.com>
2021-12-31 09:27:58 +0330

    commit

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import App from "./components/App";
+import ReactDOM from "react-dom";
+
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(
+      <App />
+  ,
+  rootElement
+);
