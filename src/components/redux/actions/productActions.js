cb299ba5a83f7c37f671ab5559c6c079a318fa5b
benyamin-nayudi <benyamin.naudi@gmail.com>
2021-12-31 09:50:04 +0330

    create reducer and combine them

---
+++ src/components/redux/actions/productActions.js
@@ -0,0 +1,19 @@
+import  { ActionsTypes } from '../constants/actionTypes'
+
+
+export const setProducts = (products) =>{
+    return {
+        type : ActionsTypes.SET_PRODUCTS ,
+        payload : products
+    }
+}
+
+export const selectedProduct = (product) =>{
+    return {
+        type : ActionsTypes.SELECTED_PRODUCT ,
+        payload : product
+    }
+}
+
+
+
--- src/components/redux/reducers/productReducer.js
+++ src/components/redux/reducers/productReducer.js
@@ -1,8 +1,19 @@
+import { ActionTypes } from "../constants/actionTypes";
 
 const initialState = {
-    products:[]
+    products:[{
+        id: 1,
+        title: 'dipesh',
+        category: 'programmer'
+    }]
 }
-export const productReducer = ( state = [] , action) => {
-    
+export const productReducer = ( state = [] , {type , payload}) => {
+    switch (type) {
+        case ActionTypes.SET_PRODUCTS:
+            return state;
+            
+        default:
+            return state;
+    }
 
 }
