b47ea4caf43796330e2f022bb5c537035b8c42ea
benyamin-nayudi <benyamin.naudi@gmail.com>
2022-01-01 10:34:38 +0330

    complete project

--- src/components/containers/ProductListing.js
+++ src/components/containers/ProductListing.js
@@ -1,32 +1,32 @@
-import {useEffect} from 'react'
-import {useSelector } from 'react-redux'
-import ProductComponent from './ProductComponent';
-import axios from 'axios'
-import { useDispatch } from 'react-redux';
-import {setProducts} from '../redux/actions/productActions'
+import axios from "axios";
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { setProducts } from "../redux/actions/productActions"; 
+import ProductComponent from "./ProductComponent";
 
+console.log(setProducts)
+const ProductPage = () => {
+  const products = useSelector((state) => state.allProducts.products);
+  const dispatch = useDispatch();
+  const fetchProducts = async () => {
+    const response = await axios
+      .get("https://fakestoreapi.com/products")
+      .catch((err) => {
+        console.log("Err: ", err);
+      });
+    dispatch(setProducts(response.data));
+  };
 
-const ProductListing = () => {
-    const products = useSelector( state => state)
-    const dispatch = useDispatch()
+  useEffect(() => {
+    fetchProducts();
+  }, []);
 
+  console.log("Products :", products);
+  return (
+    <div className="ui grid container">
+      <ProductComponent />
+    </div>
+  );
+};
 
-    const fetchData = async()=>{
-        const response = await axios.get('https://fakestoreapi.com/products').catch(err => console.log(err))
-        dispatch(setProducts(response.data))
-    }
-    
-    useEffect(()=>{
-        fetchData()
-    } ,[])
-
-
-    return ( 
-        <div className="ui grid container">
-            <ProductComponent />
-        </div>
-     );
-}
-
-
-export default (ProductListing);
+export default ProductPage;
--- src/components/redux/reducers/index.js
+++ src/components/redux/reducers/index.js
@@ -1,10 +1,10 @@
 import {combineReducers } from 'redux'
-import { productReducer  , selectedProductReducer} from './productReducer'
+import { productsReducer  , selectedProductsReducer} from './productReducer'
 
 
  const reducers = combineReducers({
-    allProducts : productReducer,
-    product : selectedProductReducer
+    allProducts : productsReducer,
+    product : selectedProductsReducer
 
 })
 
