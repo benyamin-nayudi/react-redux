bb67e6108bdb6e78925d5d5a3a405f661f93711b
benyamin-nayudi <benyamin.naudi@gmail.com>
2022-01-01 07:44:48 +0330

    routing

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,21 @@
+
+import Header from "./containers/Header";
+import {BrowserRouter as Router , Routes , Route } from 'react-router-dom'
+import ProductListing from "./containers/ProductListing";
+import ProductDetails from "./containers/ProductDetails";
+
+
 const App = () => {
     return ( 
 
         <div>
-            hi
+            <Header  />
+            <Router>
+                <Routes>
+                    <Route path="/" element={<ProductListing />} />
+                    <Route path="/product/:productId" element={<ProductDetails />} />
+                </Routes>
+            </Router>
         </div>
      );
 }
